2b4bdf5f82dd9a53006c1b211f284819079cbddb
Paulo Galvão <paulo.galvao@estsetubal.ips.pt>
2023-05-02 13:18:15 +0100

    Add files via upload

--- kivymd_button.py
+++ kivymd_button.py
@@ -4,7 +4,7 @@ from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivymd.uix.button import MDFillRoundFlatIconButton
 from kivymd.uix.textfield import MDTextField
-#from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
 
 KV = '''
 BoxLayout:
@@ -23,6 +23,7 @@ BoxLayout:
             height: self.texture_size[1]
 
         MDTextField:
+            id: name
             hint_text: 'Write your name'
             size_hint_y: None
             height: self.minimum_height
@@ -33,6 +34,7 @@ BoxLayout:
             height: self.texture_size[1]
 
         MDTextField:
+            id: surname
             hint_text: 'Write your surname'
             size_hint_y: None
             height: self.minimum_height
@@ -54,25 +56,36 @@ BoxLayout:
             icon: 'eraser'
             text: 'Clear'
             on_press: app.clear_data()
-                
 
+    MDLabel:
+        id: name_surname_label
+        text: ''
+        size_hint_y: None
+        height: self.texture_size[1]
+        halign: 'center'
+        valign: 'center'
 '''
 
 class MyApp(MDApp):
-    title="Botões"
-    Window.size = (300, 300)
-    
+    title = "Buttons"
+    Window.size = (300, 400)
+
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "Orange"
-        return Builder.load_string(KV)
+        self.root = Builder.load_string(KV)
+        return self.root
 
     def save_data(self):
-        # Code to save data goes here
-        pass
-    
+        name = self.root.ids.name.text
+        surname = self.root.ids.surname.text
+        name_surname_label = self.root.ids.name_surname_label
+        name_surname_label.text = f"{name} {surname}"
+
     def clear_data(self):
-        # Code to save data goes here
-        pass
+        self.root.ids.name.text = ''
+        self.root.ids.surname.text = ''
+        name_surname_label = self.root.ids.name_surname_label
+        name_surname_label.text = ''
 
 MyApp().run()
